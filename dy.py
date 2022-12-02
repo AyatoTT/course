2f14c060402b3126f203b2e22a0516e68b6dcd4e
Ayato-TT <vlad.tyurin.2003@mail.ru>
2022-12-02 23:38:30 +0300

    5com

--- dy.py
+++ dy.py
@@ -48,6 +48,7 @@ def printm(mat,n):
     return
 """""
 def printm(mtx, n):
+    print()
     print('    ', end=' ')
     [print("%2d" % i, end=' ') for i in range(0, len(mtx))]
     print()
@@ -58,6 +59,46 @@ def printm(mtx, n):
         for col in row:
             print("%2d" % col, end=' ')
         print()
+
+def print2file(gg,T):
+    fout = open('Dxtra.txt', 'w')  # 'w' - это режим "запись" ("write")
+    #for i in range(len(gg)):
+        #print(gg[i],file=fout)
+    print(file=fout)
+    print('    ', end=' ',file=fout)
+    [print("%2d" % i, end=' ',file=fout) for i in range(0, len(gg))]
+    print(file=fout)
+    print('-' * (len(gg[0]) * 3 + 4),file=fout)
+    for i, row in enumerate(gg):
+        print("%2d |" % (i), end=' ',file=fout  )
+        for col in row:
+            print("%2d" % col, end=' ',file=fout)
+        print(file=fout)
+    print(file=fout)
+    print("Data on shortest paths from a given point",file=fout)
+    for i in range(N):
+     print("%3d" %  i, end=" " , file=fout)
+    print(file=fout)
+    for i in range(len(T)):
+        if (T[i]== math.inf) or (T[i]==0):
+            print("","np",end=" ",file=fout)
+        else:
+            print("%3d" % T[i],end=" ",file=fout)
+    fout.close()
+    return
+
+def printresult(N,T):
+    print()
+    for i in range(N):
+     print("%3d" % i, end=" ")
+
+    print()
+    for i in range(len(T)):
+        if (T[i]== math.inf) or (T[i]==0):
+            print("","np",end=" ")
+        else:
+            print("%3d" % T[i],end=" ")
+    return
 
 while True:
     try:
@@ -75,6 +116,7 @@ T = [math.inf]*N
 def start():
     while True:
         try:
+            print()
             v=int(input("Введите стартовую вершину: "))         # стартовая вершина (нумерация с нуля)
         except:
             print("Введите число")
@@ -84,8 +126,8 @@ def start():
 
 v=start()
 
-while v>n:
-    print("Введите существующую вершину: ")
+while v>n or v==n :
+    print("Введите существующую вершину!!! ")
     v=start()
 
 S = {v}         # просмотренные вершины
@@ -105,28 +147,5 @@ while v != -1:          # цикл, пока не просмотрим все в
     if v >= 0:                    # выбрана очередная вершина
         S.add(v)                 # добавляем новую вершину в рассмотрение
 
-print()
-for i in range(N):
-    print("%3d" % i, end=" ")
-
-print()
-for i in range(len(T)):
-    if (T[i]== math.inf) or (T[i]==0):
-        print("","np",end=" ")
-    else:
-        print("%3d" % T[i],end=" ")
-
-fout = open('Dxtra.txt', 'w')  # 'w' - это режим "запись" ("write")
-for i in range(len(gg)):
-    print(gg[i],file=fout)
-print(file=fout)
-print("Data on shortest paths from a given point",file=fout)
-for i in range(N):
-    print("%3d" %  i, end=" " , file=fout)
-print(file=fout)
-for i in range(len(T)):
-    if (T[i]== math.inf) or (T[i]==0):
-        print("","np",end=" ",file=fout)
-    else:
-        print("%3d" % T[i],end=" ",file=fout)
-fout.close()
+printresult(N,T)
+print2file(gg,T)
